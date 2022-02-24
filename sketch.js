6610b234f0f11912a8acef6d468689a7488cefa2
The-Absolute-Coder <92525936+The-Absolute-Coder@users.noreply.github.com>
2022-02-24 19:27:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,9 +21,9 @@ function setup() {
   var option={
     isStatic:true
   };
-  ground=Bodies.rectangle(100,300,400,20);
+  ground=Bodies.rectangle(100,300,400,20,option);
   World.add(world,ground);
-  box1 = new Box(200,100,50,50);
+  box1 = new Box(mouseX,mouseY,50,50);
   
 
   rectMode(CENTER);
